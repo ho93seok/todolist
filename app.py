a43595b84c423333ccfd4feac911c472c23e0362
Aaron Bolton <aaron@boltonstudios.com>
2023-04-16 11:39:15 -0400

    Created some initial template files with placeholder content.

--- app.py
+++ app.py
@@ -4,3 +4,41 @@ By: Hoseok Lee, Ryan Hunt, Shanna Owens, Aaron Bolton
 CMSC 495, Spring 2023
 University of Maryland Global Campus
 """
+
+# Import dependencies.
+import datetime
+import json
+import os
+import re
+import logging
+from functools import wraps
+from flask import Flask, Markup, session, render_template, flash, request, redirect, url_for
+from passlib.hash import sha256_crypt
+
+# Define globals
+app = Flask(__name__)
+app.secret_key = os.urandom(16)
+
+# Define routes.
+@app.route('/')
+def index():
+
+    '''Home Page'''
+
+    # Initialize variables.
+    site_title = 'Home - Group 4 To-Do List Application'
+    loggedin = False
+    username = ''
+
+    # Check if the user is logged in.
+    if 'username' in session:
+        loggedin = True
+        username = session['username']
+
+    # Render template.
+    return render_template(
+        'home.html',
+        site_title = site_title,
+        site_description = "A to-do list application by Group 4.",
+        page_title = 'Welcome'
+    )
