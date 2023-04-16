29f8efad4f38c319face95019dd0a001a356e1c6
Hoseok Lee <ho93suk@gmail.com>
2023-04-17 01:45:45 +0900

    Test

--- app.py
+++ app.py
@@ -4,3 +4,27 @@ By: Hoseok Lee, Ryan Hunt, Shanna Owens, Aaron Bolton
 CMSC 495, Spring 2023
 University of Maryland Global Campus
 """
+
+from flask import Flask, render_template, request, redirect, url_for
+import csv, os
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login', methods=["GET", "POST"])
+def signin():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if username == 'admin' and password == 'password':
+            session['logged_in'] = True
+            return redirect(url_for('home'))
+        else:
+            return 'Invalid login'
+    return render_template('signin.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
