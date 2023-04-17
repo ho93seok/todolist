cfb1d59523b6a4dbc73e63c3a8d8796e526f43ae
Hoseok Lee <ho93suk@gmail.com>
2023-04-17 18:51:42 +0900

    Main merged

--- app.py
+++ app.py
@@ -5,34 +5,9 @@ CMSC 495, Spring 2023
 University of Maryland Global Campus
 """
 
-<<<<<<< HEAD
-from flask import Flask, render_template, request, redirect, url_for
-import csv, os
-
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/login', methods=["GET", "POST"])
-def signin():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        if username == 'admin' and password == 'password':
-            session['logged_in'] = True
-            return redirect(url_for('home'))
-        else:
-            return 'Invalid login'
-    return render_template('signin.html')
-
-if __name__ == '__main__':
-    app.run(debug=True)
-=======
 # Import dependencies.
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 # Define globals
 app = Flask(__name__)
@@ -54,4 +29,7 @@ def index():
         site_description = "A to-do list application by Group 4.",
         page_title = 'Welcome'
     )
->>>>>>> 40193186245dc71df08baa9599ff8fbc84db94dc
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
