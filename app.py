bacf4e77418cc385a073eada3a03dc50b710c04c
shanna-owens <Shanna.Owens@charter.com>
2023-04-17 12:17:42 -0400

    updated and added delete confirmation page

--- app.py
+++ app.py
@@ -40,43 +40,43 @@ def register():
         page_title = 'Register'
     )
 
-@app.route('/security_questions')
+@app.route('/security-questions')
 def security_questions():
 
     '''Registration Security Questions'''
 
     return render_template(
-        'security_questions.html',
+        'security-questions.html',
         page_title = 'Register'
     )
 
-@app.route('/password_update')
+@app.route('/password-update')
 def password_update():
 
     '''Password Update (loggin in)'''
 
     return render_template(
-        'password_update.html',
+        'password-update.html',
         page_title = 'Password Update'
     )
 
-@app.route('/forgot_password')
+@app.route('/forgot-password')
 def forgot_password():
 
     '''Forgot Password (logged out)'''
 
     return render_template(
-        'forgot_password.html',
+        'forgot-password.html',
         page_title = 'Forgot Password'
     )
 
-@app.route('/delete_profile', methods=["GET", "POST"])
+@app.route('/delete-profile', methods=["GET", "POST"])
 def delete_profile():
 
     '''Delete Profile'''
 
     return render_template(
-        'delete_profile.html',
+        'delete-profile.html',
         page_title = 'Delete Profile'
     )
 
